1ae25f08eacc4e43a26f7a9d46d979de1e9fad7f
root <root@srv637262.hstgr.cloud>
2025-08-05 08:31:36 +0000

    bug fixing

--- iskcongkp/settings/production.py
+++ iskcongkp/settings/production.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-ALLOWED_HOSTS = ["iyf.vishnuvahan.com","iskcongorakhpur.com","88.222.241.219"]
+ALLOWED_HOSTS = ["iskcongorakhpur.com"]
 
 
 # Application definition
