2dbd6def05c081e4fd25f4a2524337c5e3e22c0a
root <root@srv637262.hstgr.cloud>
2025-08-27 21:04:56 +0000

    adding hdfc keys

--- iskcongkp/settings/production.py
+++ iskcongkp/settings/production.py
@@ -16,6 +16,11 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
+HDFC_MERCHANT_ID = os.environ["HDFC_MERCHANT_ID"]
+HDFC_ACCESS_CODE = os.environ["HDFC_ACCESS_CODE"]
+HDFC_WORKING_KEY = os.environ["HDFC_WORKING_KEY"]
+HDFC_PAYMENT_URL = os.environ["HDFC_PAYMENT_URL"]
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
