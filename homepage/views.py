ee2cf47af8b968b4b8ea381d19e883b3c6cf4728
Vipul Kumar <66182594+VishnuVahanDas@users.noreply.github.com>
2025-08-27 18:03:04 +0530

    Add news popup modal

--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Banner, TopHeader
+from .models import Banner, TopHeader, NewsPopup
+
 
 def home_view(request):
     banners = Banner.objects.all().order_by('-id')
     top_headers = TopHeader.objects.all().order_by('-id')
+    news_popup = NewsPopup.objects.filter(active=True).order_by('-id').first()
     context = {
-         "banner": banners ,
-         "top_header": top_headers
-         }
+        "banner": banners,
+        "top_header": top_headers,
+        "news_popup": news_popup,
+    }
     return render(request, 'home.html', context)
