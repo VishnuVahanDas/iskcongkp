3471c7b5ec6e54a4af5c15b2650b73f679ddd990
root <root@srv637262.hstgr.cloud>
2025-08-30 06:28:20 +0000

    payment app started

--- iskcongkp/settings/production.py
+++ iskcongkp/settings/production.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'payments',
     'donation',
     'homepage',
     'who_we_are',
