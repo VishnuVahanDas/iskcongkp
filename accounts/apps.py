897f47cd6f566fe05fe9da3b5de0d63afc7572d5
root <root@srv637262.hstgr.cloud>
2025-09-12 07:06:52 +0000

    admin panel fixing

--- accounts/apps.py
+++ accounts/apps.py
@@ -6,3 +6,12 @@ class AccountsConfig(AppConfig):
     name = "accounts"
     verbose_name = "Accounts"
 
+    def ready(self):
+        try:
+            from django.contrib import admin
+            admin.site.site_header = "ISKCON Gorakhpur Admin Panel"
+            admin.site.site_title = "ISKCON Gorakhpur Admin"
+            admin.site.index_title = "Administration"
+        except Exception:
+            # Avoid startup issues if admin isn't loaded
+            pass
